22c223cc7d588dec5f38db2cb7d2c842dad55040
Alvin Quach <alvinwquach@gmail.com>
2023-12-05 15:01:53 -0800

    Fix styling

--- app/page.tsx
+++ app/page.tsx
@@ -40,58 +40,54 @@ export default function Home() {
 
   return (
     <main>
-      <section className="mx-auto max-w-[100rem] text-white py-16 text-center">
-        <div className="container mx-auto">
-          <Image
-            src={about.image.asset.url}
-            alt="Image of Alvin"
-            width={200}
-            height={200}
-            className="rounded-full mx-auto my-4 border border-white p-2"
-          />
+      <section className=" mx-auto max-w-[100rem] text-white py-16 text-center  mt-20 flex-grow px-4 md:px-16 lg:px-32">
+        <Image
+          src={about.image.asset.url}
+          alt="Image of Alvin"
+          width={200}
+          height={200}
+          className="rounded-full mx-auto my-4 border border-white p-2"
+        />
 
-          <div className="mx-auto flex items-center justify-center mb-5">
-            {about.linkedin && (
-              <a
-                href={about.linkedin}
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Open link to Alvin Quach's LinkedIn"
-              >
-                <FaLinkedin size={30} className="mx-2 hover:scale-110" />
-              </a>
-            )}
-            {about.github && (
-              <a
-                href={about.github}
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Open link to Alvin Quach's Github"
-              >
-                <FaGithub size={30} className="mx-2 hover:scale-110" />
-              </a>
-            )}
-            {about.email && (
-              <a
-                href={`mailto:${about.email}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Email Alvin Quach"
-              >
-                <FaEnvelope size={30} className="mx-2 hover:scale-110" />
-              </a>
-            )}
-          </div>
-          <div className="text-lg">
-            <PortableText value={about.storyRaw} />
-          </div>
+        <div className="mx-auto flex items-center justify-center mb-5">
+          {about.linkedin && (
+            <a
+              href={about.linkedin}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Open link to Alvin Quach's LinkedIn"
+            >
+              <FaLinkedin size={30} className="mx-2 hover:scale-110" />
+            </a>
+          )}
+          {about.github && (
+            <a
+              href={about.github}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Open link to Alvin Quach's Github"
+            >
+              <FaGithub size={30} className="mx-2 hover:scale-110" />
+            </a>
+          )}
+          {about.email && (
+            <a
+              href={`mailto:${about.email}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Email Alvin Quach"
+            >
+              <FaEnvelope size={30} className="mx-2 hover:scale-110" />
+            </a>
+          )}
         </div>
-      </section>
-      <div className="mx-auto max-w-[100rem] rounded-md border">
+        <div className="text-lg mb-5">
+          <PortableText value={about.storyRaw} />
+        </div>
         {sortedProjects.map((project: Project, key: number) => (
           <ProjectListItem project={project} odd={key % 2} key={project.name} />
         ))}
-      </div>
+      </section>
     </main>
   );
 }
