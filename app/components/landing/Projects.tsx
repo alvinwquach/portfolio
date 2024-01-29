76ea13fc86e1c246e1ded5335fdc57a77c7db292
Alvin Quach <alvinwquach@gmail.com>
2024-01-28 16:39:44 -0800

    Update projects section styling

--- app/components/landing/Projects.tsx
+++ app/components/landing/Projects.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Image from "next/image";
 import { PortableText } from "@portabletext/react";
 import { Project } from "@/types/Project";
+import { FiExternalLink } from "react-icons/fi";
+import { FaArrowRight, FaChevronCircleRight } from "react-icons/fa";
 
 interface ProjectsProps {
   project: Project;
@@ -11,11 +13,11 @@ interface ProjectsProps {
 function Projects({ project, odd }: ProjectsProps) {
   return (
     <div
-      className={`flex flex-col gap-x-5 p-2 transition xl:flex-row  ${
+      className={`bg-white dark:bg-gray-900 flex flex-col gap-x-5 p-2 transition xl:flex-row   ${
         odd && "xl:flex-row-reverse "
       }`}
     >
-      <div className="w-full xl:w-1/2">
+      <div className="w-full xl:w-5/12 ">
         <div className="relative aspect-[16/9]">
           <Image
             src={project.wideScreenView.asset.url}
@@ -25,53 +27,55 @@ function Projects({ project, odd }: ProjectsProps) {
             blurDataURL={project.wideScreenView.asset.url}
             placeholder="blur"
             priority
+            className="rounded-lg"
             style={{
               objectFit: "cover",
             }}
           />
         </div>
       </div>
-      <div className="flex xl:w-1/2 flex-col">
+      <div className="flex xl:w-7/12 flex-col ">
         <div className="relative mt-2 flex w-full flex-col justify-between p-3 xl:mt-0 flex-grow">
           <div className="font-serif">
-            <div className="mb-2 text-xl font-extrabold tracking-tight md:text-3xl">
+            <h2 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">
               {project.name}
-            </div>
-            <div className="text-xl ">
-              <PortableText value={project.descriptionRaw} />
-            </div>
-            <div className="mt-2 flex flex-wrap gap-x-4 gap-y-4">
-              {project.tags?.map((tag, key) => (
-                <div
-                  key={key}
-                  className="text-sm font-medium md:text-lg bg-gray-200 rounded-md px-1 py-1.5 text-black"
-                >
-                  {tag}
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="space-x-4 mt-2 flex md:flex-row">
+            </h2>
             {project.url && (
               <a
                 href={project.url}
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label={`Open link to ${project.name} website`}
-                className="inline-block px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 hover:border border-gray-400 transition duration-300 ease-in-out"
+                className="inline-flex items-center text-blue-600 hover:underline"
               >
-                Website
+                {project.url}
+                <FiExternalLink className="w-3 h-3 ms-2.5" />
               </a>
             )}
+
+            <div className="flex flex-wrap gap-4 mt-2">
+              {project.tags?.map((tag, key) => (
+                <div
+                  key={key}
+                  className="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-blue-400 mb-2"
+                >
+                  {tag}
+                </div>
+              ))}
+            </div>
+            <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
+              <PortableText value={project.descriptionRaw} />
+            </p>
             {project.repository && (
               <a
                 href={project.repository}
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label={`Open link to ${project.name} Github repository`}
-                className="inline-block px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 hover:border border-gray-400 transition duration-300 ease-in-out"
+                className="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center"
               >
-                Repository
+                View repository
+                <FaArrowRight className="inline-block ml-2" />
               </a>
             )}
           </div>
