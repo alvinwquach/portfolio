93b5b158b9733f8314c60ff53c5c8fd2d18e1c98
Alvin Quach <alvinwquach@gmail.com>
2025-09-05 17:55:59 -0700

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    turbo: {
-      rules: {},
-    },
-  },
   transpilePackages: ["@sanity/client", "@sanity/visual-editing"],
 };
 
