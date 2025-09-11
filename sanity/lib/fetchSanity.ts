656e4c826014b774503c50549107d3e0e5251fb3
Alvin Quach <alvinwquach@gmail.com>
2025-09-10 17:39:25 -0700

    Add sanity api read token to fetch sanity

--- sanity/lib/fetchSanity.ts
+++ sanity/lib/fetchSanity.ts
@@ -4,7 +4,16 @@ const apiVersion = "2025-01-01"; // Sanity API version
 const baseUrl = `https://${projectId}.api.sanity.io/v${apiVersion}/data/query/${dataset}`;
 
 export async function fetchSanity<T>(query: string): Promise<T> {
-  const res = await fetch(`${baseUrl}?query=${encodeURIComponent(query)}`);
+  const res = await fetch(`${baseUrl}?query=${encodeURIComponent(query)}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.SANITY_API_READ_TOKEN}`,
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(`Sanity fetch error: ${res.statusText}`);
+  }
+
   const data = await res.json();
   return data.result;
 }
